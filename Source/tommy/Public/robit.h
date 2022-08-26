6699e7e1d77b9a446c6c0cca2384c0379ef823d0
TaylorGwynn <tylergwynn23@gmail.com>
2022-08-25 20:19:09 -0400

    added Public and Private folders in Source since unreal didn't do that and gets confused creating new classes

---
+++ Source/tommy/Public/robit.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "tommyCharacter.h"
+#include "robit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOMMY_API Arobit : public AtommyCharacter
+{
+	GENERATED_BODY()
+	
+};
